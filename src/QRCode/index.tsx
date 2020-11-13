7f2e4d37cb37df9f6989bab27b828e8bfd6c6617
easonzen <scnznl@gmail.com>
2020-11-13 20:25:40 +0800

    delete Foo & QRCode add error handler

--- src/QRCode/index.tsx
+++ src/QRCode/index.tsx
@@ -1,37 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { getIsSupport } from './utils';
+import React, { useEffect } from 'react';
+import { isSupport, isMobile, isIOS, isChrome } from './utils';
 
 const QRCode = () => {
     let videoStream = null as any;
 
-    const [isSupport] = useState(getIsSupport());
-    // const [srcObject, setSrcObject] = useState();
-
     const init = async () => {
         const constraints = {
             video: {
-                width: {
-                    min: 1280,
-                    ideal: 1920,
-                    max: 2560
-                },
-                height: {
-                    min: 720,
-                    ideal: 1080,
-                    max: 1440
-                }
+                width: 414,
+                height: 736
             }
         };
 
-        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        try {
+            videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        } catch (error) {
+            alert('打开摄像头失败，请刷新页面重试');
+        }
 
         // 页面中有一个 <video autoplay id="video"></video> 标签
         const video = document.querySelector('#camera') as any;
         video.srcObject = videoStream;
 
+        // 如果<video />为设置autoPlay，需要调用video.play()方法
         video.play();
-
-        // setSrcObject(videoStream);
     };
 
     const destroy = () => {
@@ -39,6 +31,14 @@ const QRCode = () => {
             videoStream.getTracks().forEach((track: any) => {
                 track.stop();
             });
+    };
+
+    const _renderNotSupport = () => {
+        if (isMobile && isIOS && isChrome) {
+            return <div>Chrome浏览器不支持使用摄像头，请使用Safari浏览器打开此页面</div>;
+        }
+
+        return <div>当前浏览器不支持使用摄像头，请更换浏览器重新尝试</div>;
     };
 
     useEffect(() => {
@@ -49,13 +49,13 @@ const QRCode = () => {
         };
     }, []);
 
-    return isSupport ? (
+    return isSupport() ? (
         <>
             <video id="camera" />
             <button onClick={destroy}>关闭</button>
         </>
     ) : (
-        <div>ssss</div>
+        _renderNotSupport()
     );
 };
 
--- src/QRCode/utils.ts
+++ src/QRCode/utils.ts
@@ -1,7 +1,25 @@
-export const getIsSupport = () => {
+export const isSupport = () => {
     if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
         return true;
     }
 
     return false;
+};
+
+export const isMobile = () => {
+    const ua = navigator.userAgent;
+
+    return ua.match(/AppleWebKit.*Mobile.*/);
+};
+
+export const isIOS = () => {
+    const ua = navigator.userAgent;
+
+    return ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+};
+
+export const isChrome = () => {
+    const ua = navigator.userAgent;
+
+    return ua.indexOf('Chrome');
 };
